f1c6d939a6b575d9ff954b5a9780fd085efb629a
Joe <joe@versaillesheroes.com>
2022-09-24 12:51:53 +0800

    update

--- src/app/pages/recapcha/recapcha.component.ts
+++ src/app/pages/recapcha/recapcha.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RecaptchaErrorParameters } from "ng-recaptcha";
+import {AccessorService} from "../../service/accessor.service";
+
+declare const window: any;
 
 @Component({
   selector: 'app-recapcha',
@@ -8,13 +11,21 @@ import { RecaptchaErrorParameters } from "ng-recaptcha";
 })
 export class RecapchaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private accessorService: AccessorService) { }
 
   ngOnInit(): void {
   }
 
   public resolved(captchaResponse: string): void {
     console.log(`Resolved captcha with response: ${captchaResponse}`);
+
+    let faucetRequest:any = {};
+    faucetRequest.token = captchaResponse;
+
+    this.accessorService.post(window.serviceUrl + "/vrh-faucet-api/api/v1/public/getTokens", faucetRequest).subscribe((response: any)=>{
+      console.log(response);
+    });
+
   }
 
   public onError(errorDetails: RecaptchaErrorParameters): void {
