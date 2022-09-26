8bf00e5824f68c4221b3b22509707f00edc2e53f
joe <joe@versaillesheroes.com>
2022-09-26 21:22:05 +0800

    add faucet

--- src/app/pages/recapcha/recapcha.component.ts
+++ src/app/pages/recapcha/recapcha.component.ts
@@ -16,20 +16,29 @@ export class RecapchaComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  address: string;
+  token: string;
+
   public resolved(captchaResponse: string): void {
     console.log(`Resolved captcha with response: ${captchaResponse}`);
-
-    let faucetRequest:any = {};
-    faucetRequest.token = captchaResponse;
-
-    this.accessorService.post(window.serviceUrl + "/vrh-faucet-api/api/v1/public/getTokens", faucetRequest).subscribe((response: any)=>{
-      console.log(response);
-    });
-
+    this.token = captchaResponse;
   }
 
   public onError(errorDetails: RecaptchaErrorParameters): void {
     console.log(`reCAPTCHA error encountered; details:`, errorDetails);
+  }
+
+  onSubmit(): void{
+    let faucetRequest:any = {};
+    faucetRequest.token = this.token;
+    faucetRequest.address = this.address;
+
+    this.accessorService.post(window.serviceUrl + "/vrh-faucet-api/api/v1/public/getTokens", faucetRequest).subscribe((response: any)=>{
+      console.log(response);
+      if(response.data != true){
+        alert(response.msg);
+      }
+    });
   }
 
 }
